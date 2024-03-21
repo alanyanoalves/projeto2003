bed2a98a85f2c71ae3daaf139f4181ab146046c8
Alan Gustavo Yano Alves <alan.alves@pucpr.edu.br>
2024-03-20 21:00:36 -0300

    pagina de login

--- projeto2003/src/Main.java
+++ projeto2003/src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        System.out.println("pagina de login");
+        System.out.println("usuario : admin");
+        System.out.println("senha: *****");
         }
     }
 }
